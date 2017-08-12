34358f04134941bb7568d011ef05dac00f4e11ae
Zukka <info@zukkadev.net>
2017-08-12 15:04:37 +0200

    LoginViewController

---
+++ OnTheMap/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Alessandro Bellotti on 12/08/17.
+//  Copyright © 2017 Alessandro Bellotti. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
