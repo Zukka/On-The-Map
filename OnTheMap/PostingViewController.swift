902a32b19dad6da795e2aeebd6f3cfa6aaed0f38
Zukka <info@zukkadev.net>
2017-08-23 15:30:25 +0200

    update activity indicator

--- OnTheMap/PostingViewController.swift
+++ OnTheMap/PostingViewController.swift
@@ -23,7 +23,10 @@ class PostingViewController: UIViewController, MKMapViewDelegate, CLLocationMana
     @IBOutlet weak var submitButton: UIButton!
     @IBOutlet weak var textFieldLink: UITextField!
     @IBOutlet weak var resultMapView: MKMapView!
+    
+    // Activity Indicator
     @IBOutlet weak var geocodingActivityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var viewActivityIndicator: UIView!
     
     // MARK: Var
     var geocodeFailAlertView: UIAlertController?
@@ -62,6 +65,8 @@ class PostingViewController: UIViewController, MKMapViewDelegate, CLLocationMana
             showAlertView(message: "Place field is empty.")
             return
         }
+        // Start Activity Indicator
+        self.activityIndicatorIsHidden(hide: false)
         geocodeStringAddress(address: textFieldPlace.text!)
     }
     
@@ -115,12 +120,10 @@ class PostingViewController: UIViewController, MKMapViewDelegate, CLLocationMana
     func geocodeStringAddress(address: String) {
         let geoCoder = CLGeocoder()
         geoCoder.geocodeAddressString(address) { (placemark, error) in
-        // Start Activity Indicator
-        self.startActivityIndicator()
             performUIUpdatesOnMain {
                 if error != nil {
                     // Stop Activity Indicator
-                    self.stopActivityIndicator()
+                    self.activityIndicatorIsHidden(hide: true)
                     let messageError =  "Error: \(String(describing: error!.localizedDescription))"
                     self.showAlertView(message: messageError)
                 } else if placemark!.count > 0 {
@@ -135,7 +138,6 @@ class PostingViewController: UIViewController, MKMapViewDelegate, CLLocationMana
     
     func showMap() {
         
-        showStartLayout(show: false)
         
         //  MKCoordinateSpanMake define the extension of the  area to show
         let span = MKCoordinateSpanMake(0.1, 0.1)
@@ -149,7 +151,8 @@ class PostingViewController: UIViewController, MKMapViewDelegate, CLLocationMana
         studentAnnotation.title = "Current location"
         resultMapView.addAnnotation(studentAnnotation)
         // Stop Activity Indicator
-        self.stopActivityIndicator()
+        self.activityIndicatorIsHidden(hide: true)
+        showStartLayout(show: false)
     }
     
     func showStartLayout(show: Bool) {
@@ -159,16 +162,12 @@ class PostingViewController: UIViewController, MKMapViewDelegate, CLLocationMana
     
     // MARK : Activity Indicator
     
-    func startActivityIndicator() {
-        geocodingActivityIndicator.isHidden = false
+    func activityIndicatorIsHidden(hide : Bool) {
+        viewActivityIndicator.isHidden = hide
+        geocodingActivityIndicator.isHidden = hide
         geocodingActivityIndicator.startAnimating()
     }
     
-    func stopActivityIndicator() {
-        geocodingActivityIndicator.isHidden = true
-        geocodingActivityIndicator.stopAnimating()
-    }
-
 }
 
 // MARK: - PostingViewController Alerts
